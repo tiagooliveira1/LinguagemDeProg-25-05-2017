47a6dcbd91389a2a2093150c4691a074a23ca96e
Tiago  Oliveira <tiago@econtabil.com>
2017-05-25 21:30:32 -0300

    Inserido código SQL para cadastro da tabela

--- src/com/teste/Aula25_05_17_Exercicio1.java
+++ src/com/teste/Aula25_05_17_Exercicio1.java
@@ -1,11 +1,40 @@
 package com.teste;
 
+import com.opet.util.Reader;
+
 public class Aula25_05_17_Exercicio1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("asdasdasdas");
+	public static void main(String[] args) throws Exception {
+		int opcao = 0;
+		while (opcao != 9) {
+			System.out.println("Informe a sua opção:");
+			System.out.println("1) Inserir aluno.");
+			System.out.println("2) Alterar informações.");
+			System.out.println("3) Excluir aluno.");
+			System.out.println("4) Listar alunos.");
+			System.out.println("9) Sair.");
+			opcao = Reader.readInt();
+			
+			switch (opcao) {
+			case 1:
+				System.out.println("Informe o nome do aluno:");
+				String nomeAluno;
+				Double nota1, nota2;
+				nomeAluno = Reader.readString();
+				System.out.println("Informe a primeira nota:");
+				nota1 = Reader.readDouble();
+				System.out.println("Informe a segunda nota:");
+				nota2 = Reader.readDouble();
+				
+				aluno objAluno = new aluno();
+				aluno.inserirAluno(nomeAluno, nota1, nota2);
+				break;
 
+			default:
+				break;
+			} 
+		}
+		System.out.println("Bye bye!");
 	}
 
 }
---
+++ src/com/teste/aluno.java
@@ -0,0 +1,8 @@
+package com.teste;
+
+public class aluno {
+
+	public static void inserirAluno (String nomeAluno, Double Nota1, Double Nota2 ) {
+		
+	}
+}
